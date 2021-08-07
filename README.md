125ecb20ee10b2a26977e986cf54f7cd032f6d92
Alexandr Degteari <alexdevelopermd@gmail.com>
2021-08-07 13:37:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# What is this?
+
+This is a change return algorithm for a vending machine. It is universal in terms of coin
+values and it has infinite amount of coins of all types. However for the user it is crucial that the machine returns
+smallest amount of coin possible.
+
+# Installation
+
+`npm i coin --save`
+
+
+
+```
+import { coin } from 'coin';
+
+coin({
+  coins: '[1, 2, 5]',
+  total: 12
+})
+```
+
+## **Parameters**
+
+We have 2 required parameters: 
+
+- _coins_ - _[1, 2, 5]_ (array of available coins, element 1 is required)
+- _total_ - _integer_ (amount of rest)
---
+++ index.js
@@ -0,0 +1,35 @@
+const coin = (params) => {
+    // Validation
+    if (!Number.isInteger(params.total)) {
+        return 'Error params.total must be integer!'
+    }
+    if (params.coins.some(isNaN)) {
+        return 'Error params.coins must be array of integers!'
+    }
+    if (!params.coins.some(1)) {
+        return 'Element 1 is required!'
+    }
+    // Sorting
+    params.coins = params.coins.sort((a, b) => b - a);
+    let rest = params.total;
+    let result = '';
+    params.coins.forEach(function callback(currentValue) {
+        if (rest === 0) {
+            return;
+        }
+        // Calculation of rest
+        let currentCoinNumber = Math.trunc(rest / currentValue);
+        if (currentCoinNumber) {
+            rest -= currentCoinNumber * currentValue;
+            for (let i = 0; i < currentCoinNumber; i++) {
+                result += currentValue + ',';
+            }
+        }
+    });
+    if (rest > 0) {
+        return 'Rest cannot be issued!'
+    }
+    return result.substring(0, result.length - 1);
+}
+module.exports.coin = coin;
+
